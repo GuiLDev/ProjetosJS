dc0d54a52d7d5d575706ad03032ca9c81f04dd72
GuilhermeL <guilhermerebelo.dev@gmail.com>
2024-08-01 11:25:58 -0400

    Separação da pasta

--- Treinamento/mdn 1/index.js
+++ Treinamento/mdn 1/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded");
+
   document.querySelector("button").addEventListener("click", function (event) {
     document.querySelectorAll("p").forEach(function (paragraph) {
-      paragraph.classList.toggle("caldeirao");
-    });
+      paragraph.classList.toggle("caldeirao");  
+  });
     var newButtonText = event.target.dataset.toggleText;
     var oldText = event.target.innerText;
     event.target.innerText = newButtonText;
