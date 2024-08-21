3641bb217a0e3e164cccc8f26bb5cc8c8909236b
GuilhermeL <guilhermerebelo.dev@gmail.com>
2024-08-21 11:04:46 -0400

    Movido os projetos de HTML para uma pasta separada

---
+++ Treinamento/Treinamento HTML/mdn 1/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded");
+
+  document.querySelector("button").addEventListener("click", function (event) {
+    document.querySelectorAll("p").forEach(function (paragraph) {
+      paragraph.classList.toggle("caldeirao");  
+  });
+    var newButtonText = event.target.dataset.toggleText;
+    var oldText = event.target.innerText;
+    event.target.innerText = newButtonText;
+    event.target.dataset.toggleText = oldText;
+  });
+});
