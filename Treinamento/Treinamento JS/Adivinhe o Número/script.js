e21c2395116b3598c78d1c8e787bc3678aef5a18
Guilherme Rebelo <guilhermerebelo.dev@gmail.com>
2024-09-24 11:19:39 -0400

    Verificando ocorrencias de funções em erro

---
+++ Treinamento/Treinamento JS/Adivinhe o Número/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+const button = document.getElementsByClassName("submitGuess")
+
+button.addEventListener("click", checkGuess)
+
+function checkGuess(){
+    const userGuess = Number(inputGuess.value)
+}
+});
