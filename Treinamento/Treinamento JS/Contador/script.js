6fdaecaa945d001dde7a43a6bb49809f8cee8edc
Guilherme Rebelo <guilhermerebelo.dev@gmail.com>
2024-09-24 17:31:24 -0400

    Treinamento com logica no JS

---
+++ Treinamento/Treinamento JS/Contador/script.js
@@ -0,0 +1,20 @@
+const botaoIncremento = document.getElementsByClassName('incremento')[0];
+const botaoDecremento = document.getElementsByClassName('decremento')[0];
+const numeroContador = document.getElementsByClassName('contador')[0];
+
+let contador = 0;
+botaoDecremento.disabled = true;
+
+botaoIncremento.addEventListener('click', function(){
+    contador++; 
+    numeroContador.textContent = `Número do contador: ${contador}`;
+    if (contador > 0){
+        botaoDecremento.disabled = false;
+    }
+});
+
+
+botaoDecremento.addEventListener('click', function(){
+    contador--; 
+    numeroContador.textContent = `Número do contador: ${contador}`;
+});
